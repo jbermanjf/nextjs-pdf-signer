4cdce5d3b7aa9a0898b5efc053d967ca8fa15360
inexperienced-developer <inexperienced.developer.games@gmail.com>
2023-04-21 13:08:40 -0400

    Testing something

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    EMAIL: process.env.EMAIL,
+    TEST_EMAIL: process.env.TEST_EMAIL,
+    FIREBASE_API: process.env.FIREBASE_API,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+  }
 }
 
 module.exports = nextConfig
